e5d51ab07b92dcbdb9c95c489a937f3112021c7d
Sam <elnyry@users.noreply.github.com>
2016-12-05 13:16:43 -0700

    Develop (#1)
    * added support for basic authentication between ledger adapter and remote ledger
    
    * update to support corrected LedgerMetadata and Health output
    
    * putAccount returns 201 when creating new account, 200 when updating account
    
    * implemented request / response mapping to map private urls from ledger to public urls
    
    * fixed spelling issue in parameter name
    
    * mapping fixes to make end to end demo work
    
    * mapping fixes to make end to end demo work
    
    * mapping fixes to make end to end demo work
    
    * Metrics added to one of the files ilp-ledger.xml .  ill-ledger-proxy.xml still needs to be updated
    
    * opened up allowed response codes
    
    * opened up allowed response codes
    
    * opened up allowed response codes
    
    * Added BMGF license file
    
    * Added the path for getMetaData(empty value) as a property and some auto completes by AnyPoint Studio
    
    * Refactoring http acceptable codes
    
    * Added unit tests for most resources. For Transfer related tests, need updated code. These along with 'health' need to be finished.

---
+++ src/main/java/com/l1p/interop/ilp/ledger/LedgerMetadataTransformer.java
@@ -0,0 +1,62 @@
+package com.l1p.interop.ilp.ledger;
+
+import com.l1p.interop.JsonTransformer;
+import com.l1p.interop.L1PErrorResponse;
+
+import org.mule.api.MuleMessage;
+import org.mule.api.transformer.TransformerException;
+import org.mule.api.transport.PropertyScope;
+import org.mule.transformer.AbstractMessageTransformer;
+import org.mule.transformer.types.DataTypeFactory;
+
+import java.util.Map;
+
+/**
+ * Created by Bryan on 8/17/2016.
+ */
+public class LedgerMetadataTransformer extends AbstractMessageTransformer {
+
+    //private static final Logger logger = LogManager.getLogger(GetAccountTransformer.class);
+    final String ledgerAdapterURL;
+    final String ledgerAccountTransfersURL;
+
+    public LedgerMetadataTransformer( final String ledgerAdapterURL, final String ledgerAccountTransfersURL ) {
+        registerSourceType(DataTypeFactory.create(String.class));
+        setReturnDataType((DataTypeFactory.create(String.class)));
+        setName("ServerMetadataTransformer");
+        this.ledgerAdapterURL = ledgerAdapterURL;
+        this.ledgerAccountTransfersURL = ledgerAccountTransfersURL;
+    }
+
+    /**
+     * Message Transformer to augment metadata response returned by ledger to include paths to the ledger adapter for
+     * supported functions
+     *
+     * @param muleMessage
+     * @param s
+     * @return
+     * @throws TransformerException
+     */
+    public Object transformMessage(MuleMessage muleMessage, String s) throws TransformerException {
+        Map<String,Object> jsonPayload = JsonTransformer.stringToMap( (String)muleMessage.getPayload() );
+
+        Map<String,Object> urls = (Map<String,Object>)jsonPayload.get( "urls" );
+        updateProperty( "health", ledgerAdapterURL + "/health", urls );
+        updateProperty( "transfer", ledgerAdapterURL + "/transfers/:id", urls );
+        updateProperty( "transfer_fulfillment", ledgerAdapterURL + "/transfers/:id/fulfillment", urls );
+        updateProperty( "transfer_state", ledgerAdapterURL + "/transfers/:id/state", urls );
+        updateProperty( "accounts", ledgerAdapterURL + "/accounts", urls );
+        updateProperty( "account", ledgerAdapterURL + "/accounts/:name", urls );
+        urls.put( "transfer_rejection", ledgerAdapterURL + "/transfers/:id/rejection" );
+
+        urls.put("account_transfers", ledgerAccountTransfersURL);
+
+        return JsonTransformer.mapToString( jsonPayload );
+    }
+
+    private void updateProperty( String key, String value, Map<String,Object> target ) {
+        if ( target.containsKey( key ) ) {
+            target.put( key, value );
+        }
+    }
+}
--- src/test/java/com/l1p/interop/ilp/ledger/ILPLedgerAdapterFunctionalTest.java
+++ src/test/java/com/l1p/interop/ilp/ledger/ILPLedgerAdapterFunctionalTest.java
@@ -13,6 +13,9 @@ import java.security.cert.CertificateException;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.google.common.net.MediaType;
+import com.l1p.interop.JsonTransformer;
+
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -29,8 +32,8 @@ import org.slf4j.LoggerFactory;
 
 public class ILPLedgerAdapterFunctionalTest extends FunctionalTestCase {
 
-	private final String accountsPath="/ilp/ledger/v1/accounts";
-	private final String transfersPath="/ilp/ledger/v1/transfers";
+	private final String accountsPath="/ilp/ledger/v1/accounts/";
+	private final String transfersPath="/ilp/ledger/v1/transfers/";
 	private final String connectorsPath="/ilp/ledger/v1/connectors";
 	private final String serviceHost = "http://localhost:8081";
 
@@ -67,12 +70,151 @@ public class ILPLedgerAdapterFunctionalTest extends FunctionalTestCase {
 	}
 
 	@Test
-	public void testValidPutAccountShouldReturnValidResponse() throws Exception {
+	public void testPutAccounts() throws Exception {
 		final String putAccountJSON = loadResourceAsString("testData/putAccount-Alice.json");
 		Map<String, String> params = new HashMap<String,String>();
 		params.put( "Authorization", "Basic YWRtaW46Zm9v" );
+		String id = "alice";
 
-		ClientResponse clientResponse = putRequestWithQueryParams( accountsPath + "/alice", params, putAccountJSON );
+		//Invalid PUT duplicate - invalid because record already exists
+		ClientResponse clientResponse = putRequestWithQueryParams( accountsPath + id, params, putAccountJSON );
+		Map<String, Object> jsonReponse = JsonTransformer.stringToMap( clientResponse.getEntity(String.class) );
+		assertEquals( "AccountsPutInValid" + ": Did not receive status 400", 400, clientResponse.getStatus());
+		assertEquals( "Response field id did not contain expected value", "PortPostgreSQL", jsonReponse.get( "id" ) );
+		assertEquals( "Response field message did not contain expected value", "duplicate key value violates unique constraint \"ukLedgerAccountAccountNumber\"", jsonReponse.get( "message" ) );
+		
+		//Valid PUT, to create a new ID every time the test runs, using random
+		//This test is working fine but disabling this currently, because every time this test runs, an account is added,
+		//I don't mind it but it appears they are being saved and I'm not sure about the cleanup mechanism of that DB table, so run this only when needed
+		/*
+		final String testId = java.util.UUID.randomUUID().toString();
+		boolean is_disabled = false;
+		clientResponse = putRequestWithQueryParams( accountsPath + testId, params, putAccountJSON );
+		jsonReponse = JsonTransformer.stringToMap( clientResponse.getEntity(String.class) );
+		assertEquals( "AccountsPutValid" + ": Did not receive status 200", 200, clientResponse.getStatus());
+		assertEquals( "Response field name did not contain expected value", testId, jsonReponse.get( "name" ) );
+		assertEquals( "Response field balance did not contain expected value", "1000.00", jsonReponse.get( "balance" ) );
+		assertEquals( "Response field is_disabled did not contain expected value", is_disabled, jsonReponse.get( "is_disabled" ) );
+		assertTrue( "response field is_disabled was not present in resposne", jsonReponse.get( "id") != null );
+		*/
+		
+		//Invalid PUT, empty json body
+		final String testId = java.util.UUID.randomUUID().toString();
+		clientResponse = putRequestWithQueryParams( accountsPath + testId, params, "" );
+		jsonReponse = JsonTransformer.stringToMap( clientResponse.getEntity(String.class) );
+		assertEquals( "AccountsPutInValidEmptyBody" + ": Did not receive status 400", 400, clientResponse.getStatus());
+		assertEquals( "Response field statusCode did not contain expected value", 400, jsonReponse.get( "statusCode" ) );
+		assertEquals( "Response field error did not contain expected value", "Bad Request", jsonReponse.get( "error" ) );
+		assertEquals( "Response field message did not contain expected value", "Invalid request payload JSON format", jsonReponse.get( "message" ) );
+		
+		//Invalid PUT, no authorization
+		//TODO - Disabling this because, currently account is getting created without auth :-), should this be changed?
+		/*
+		params.clear();
+		clientResponse = putRequestWithQueryParams( accountsPath + testId, params, putAccountJSON );
+		jsonReponse = JsonTransformer.stringToMap( clientResponse.getEntity(String.class) );
+		assertEquals( "AccountsPutInvalidNoAuth" + ": Did not receive status 404", 404, clientResponse.getStatus());
+		*/
+
+	}
+
+	@Test
+	public void testGetAccounts() throws Exception {
+		
+		Map<String, String> params = new HashMap<String,String>();
+		params.put( "Authorization", "Basic YWRtaW46Zm9v" );
+		//This account should already be present (considering that account 'alice' exists)
+		String id = "alice";
+		String is_disabled = "1";
+		
+		//Valid GET for account alice
+		ClientResponse clientResponse = getRequest( accountsPath + id, params );
+		Map<String, Object> jsonReponse = JsonTransformer.stringToMap( clientResponse.getEntity(String.class) );
+		assertEquals( "AccountsGetValid" + ": Did not receive status 200", 200, clientResponse.getStatus());
+		assertEquals( "Response field name did not contain expected value", id, jsonReponse.get( "name" ) );
+		assertEquals( "Response field is_disabled did not contain expected value", is_disabled, jsonReponse.get( "is_disabled" ) );
+		assertTrue( "response field is_disabled was not present in resposne", jsonReponse.get( "id") != null );
+		assertTrue( "response field balance was not present in resposne", jsonReponse.get( "balance") != null );
+		assertTrue( "response field balance was not present in resposne", jsonReponse.get( "ledger") != null );
+		
+		//InValid GET for account that doesn't exist
+		String testId = java.util.UUID.randomUUID().toString() + java.util.UUID.randomUUID().toString();
+		clientResponse = getRequest( accountsPath + testId, params );
+		jsonReponse = JsonTransformer.stringToMap( clientResponse.getEntity(String.class) );
+		assertEquals( "AccountsGetInValid" + ": Did not receive status 404", 404, clientResponse.getStatus());
+		assertEquals( "Response field id did not contain expected value", "NotFoundError", jsonReponse.get( "id" ) );
+		assertEquals( "Response field message did not contain expected value", "Unknown account.", jsonReponse.get( "message" ) );
+		
+	}
+
+	@Test
+	public void testGetHealth() throws Exception {
+		//TODO: Finish this when /health works as expected, currently getting a 404
+		ClientResponse clientResponse = getRequest( "/ilp/ledger/v1/health", null );
+		String responseContent = null;
+		try {
+			responseContent = clientResponse.getEntity(String.class);
+		} catch ( Exception e ) {
+			fail( "parsing client response content produced an unexpected exception: " + e.getMessage() );
+		}
+	}
+
+	@Test
+	public void testRejectTransferShouldReturnValidResponse() throws Exception {
+		ClientResponse clientResponse = putRequestWithQueryParams( "/ilp/ledger/v1/transfers/12345/rejection", null, "cf:0:_v8" );
+		String responseContent = null;
+		try {
+			responseContent = clientResponse.getEntity(String.class);
+		} catch ( Exception e ) {
+			fail( "parsing client response content produced an unexpected exception: " + e.getMessage() );
+		}
+	}
+
+	@Test
+	public void testGetMetadata() throws Exception {
+		
+		Map<String, String> params = new HashMap<String,String>();
+		params.put( "Authorization", "Basic YWRtaW46Zm9v" );
+		
+		//Valid GET request for Metadata
+		ClientResponse clientResponse = getRequest( "/ilp/ledger/v1/", params );
+		Map<String, Object> jsonReponse = JsonTransformer.stringToMap( clientResponse.getEntity(String.class) );
+		Map<String, Object> urls = (Map<String, Object>) jsonReponse.get( "urls");
+		
+		assertEquals( "MetadataGetValid" + ": Did not receive status 200", 200, clientResponse.getStatus());
+		assertEquals( "Response field precision did not contain expected value", 10, jsonReponse.get( "precision" ) );
+		assertEquals( "Response field scale did not contain expected value", 2, jsonReponse.get( "scale" ) );
+		
+		assertTrue( "response field currency_code was not present in response", jsonReponse.get( "currency_code") == null );
+		assertTrue( "response field currency_symbol was not present in response", jsonReponse.get( "currency_symbol") == null );
+		assertTrue( "response field condition_sign_public_key was not present in response", jsonReponse.get( "condition_sign_public_key") != null );
+		assertTrue( "response field notification_sign_public_key was not present in response", jsonReponse.get( "notification_sign_public_key") != null );
+		assertTrue( "response field urls was not present in response", jsonReponse.get( "urls") != null );
+		
+		if ( jsonReponse.get( "urls") != null )	{
+			assertTrue( "response field transfer was not present in response", urls.get( "transfer") != null );
+			assertTrue( "response field transfer_fulfillment was not present in response", urls.get( "transfer_fulfillment") != null );
+			assertTrue( "response field transfer_state was not present in response", urls.get( "transfer_state") != null );
+			assertTrue( "response field accounts was not present in response", urls.get( "accounts") != null );
+			assertTrue( "response field account was not present in response", urls.get( "account") != null );
+			assertTrue( "response field account_transfers was not present in response", urls.get( "account_transfers") != null );
+			assertTrue( "response field subscription was not present in response", urls.get( "subscription") != null );
+			assertTrue( "response field transfer_rejection was not present in response", urls.get( "transfer_rejection") != null );
+		}
+		
+	}
+
+	//Transfer related propose, prepare and execute need up-to-date code of the whole project, then need to be executed in an order
+	//The tests below can be finished once that is done.
+	@Test
+	public void testPutTransferFulfillment() throws Exception {
+		Map<String, String> params = new HashMap<String,String>();
+		params.put( "Authorization", "Basic YWRtaW46Zm9v" );
+		String id = "3a2a1d9e-8640-4d2d-b06c-84f2cd613204";
+		final String putTransferJSON = loadResourceAsString("testData/proposeTransferBody.json");
+
+		ClientResponse clientResponse = putRequestWithQueryParamsNullContentType( transfersPath + id, params, putTransferJSON );
+		//ClientResponse clientResponse = putRequestWithQueryParamsNullContentType( transfersPath + id + "/fulfillment", params, putTransferJSON );
 		String responseContent = null;
 		try {
 			responseContent = clientResponse.getEntity(String.class);
@@ -83,65 +225,29 @@ public class ILPLedgerAdapterFunctionalTest extends FunctionalTestCase {
 		System.out.println( "=== response content: " + responseContent );
 
 	}
-
+	
 	@Test
-	public void testValidGetAccountsShouldReturnValidResponse() throws Exception {
-		ClientResponse clientResponse = getRequest( accountsPath, null );
-		String responseContent = null;
-		try {
-			responseContent = clientResponse.getEntity(String.class);
-		} catch ( Exception e ) {
-			fail( "parsing client response content produced an unexpected exception: " + e.getMessage() );
-		}
-
-		System.out.println( "=== response content: " + responseContent );
-
+	public void testGetTransfer() throws Exception {
+		Map<String, String> params = new HashMap<String,String>();
+		params.put( "Authorization", "Basic YWRtaW46Zm9v" );
+		String id = "3a2a1d9e-8640-4d2d-b06c-84f2cd613";
+		
+		//http.status=200: {"id":"http://ec2-52-37-54-209.us-west-2.compute.amazonaws.com:8088/ledger/transfers/undefined","ledger":"http://ec2-52-37-54-209.us-west-2.compute.amazonaws.com:8088/ledger","debits":[{"account":"http://ec2-52-37-54-209.us-west-2.compute.amazonaws.com:8088/ledger/accounts/alice","amount":"50.00"}],"credits":[{"account":"http://ec2-52-37-54-209.us-west-2.compute.amazonaws.com:8088/ledger/accounts/bob","amount":"50.00"}],"execution_condition":"cc:0:3:8ZdpKBDUV-KX_OnFZTsCWB_5mlCFI3DynX5f5H2dN-Y:2","cancellation_condition":null,"expires_at":"2016-11-27T00:00:01.000Z","state":"proposed","timeline":{"proposed_at":"2016-11-04T05:23:20.940Z","prepared_at":"2016-11-04T05:23:20.940Z","executed_at":null}}
+		//"params":{"pattern":{},"value":"3a2a1d9e-8640-4d2d-b06c-84f2cd613","key":"id"}}]}
+		//Invalid Get Transfer - Transfer not found
+		ClientResponse clientResponse = getRequest( transfersPath + id, params );
+		Map<String, Object> jsonReponse = JsonTransformer.stringToMap( clientResponse.getEntity(String.class) );
+		assertEquals( "TransferGetInValid" + ": Did not receive status 200", 200, clientResponse.getStatus());
+		assertEquals( "Response field id did not contain expected value", "InvalidUriParameterError", jsonReponse.get( "id" ) );
+		assertEquals( "Response field message did not contain expected value", "id is not a valid Uuid", jsonReponse.get( "message" ) );
+		assertTrue( "Response field validationErrors was not present in response", jsonReponse.get( "validationErrors" ) != null );
+		
 	}
 
 	@Test
-	public void testValidGetMetadataShouldReturnValidResponse() throws Exception {
-		ClientResponse clientResponse = getRequest( "/ilp/ledger/v1/", null );
-		String responseContent = null;
-		try {
-			responseContent = clientResponse.getEntity(String.class);
-		} catch ( Exception e ) {
-			fail( "parsing client response content produced an unexpected exception: " + e.getMessage() );
-		}
-
-		System.out.println( "=== response content: " + responseContent );
-
-	}
-
-	@Test
-	public void testValidGetAccountShouldReturnValidResponse() throws Exception {
-		ClientResponse clientResponse = getRequest( accountsPath + "/alice", null );
-		String responseContent = null;
-		try {
-			responseContent = clientResponse.getEntity(String.class);
-		} catch ( Exception e ) {
-			fail( "parsing client response content produced an unexpected exception: " + e.getMessage() );
-		}
-
-		System.out.println( "=== response content: " + responseContent );
-
-	}
-
-	@Test
-	public void testValidGetTransferShouldReturnValidResponse() throws Exception {
-		ClientResponse clientResponse = getRequest( transfersPath + "/3a2a1d9e-8640-4d2d-b06c-84f2cd613204", null );
-		String responseContent = null;
-		try {
-			responseContent = clientResponse.getEntity(String.class);
-		} catch ( Exception e ) {
-			fail( "parsing client response content produced an unexpected exception: " + e.getMessage() );
-		}
-
-		System.out.println( "=== response content: " + responseContent );
-
-	}
-
-	@Test
-	public void testValidGetConnectorsShouldReturnValidResponse() throws Exception {
+	public void testGetConnectors() throws Exception {
+		//TODO: This needs to be implemented when connectors implementation in RAML or elsewhere is finished. Couldn't find this resource as of now
+		
 		ClientResponse clientResponse = getRequest( connectorsPath, null );
 		String responseContent = null;
 		try {
@@ -149,9 +255,6 @@ public class ILPLedgerAdapterFunctionalTest extends FunctionalTestCase {
 		} catch ( Exception e ) {
 			fail( "parsing client response content produced an unexpected exception: " + e.getMessage() );
 		}
-
-		System.out.println( "=== response content: " + responseContent );
-
 	}
 
 	private void validateResponse( String testName, ClientResponse clientResponse, int expectedStatus, String expectedContent ) throws Exception {
@@ -186,6 +289,7 @@ public class ILPLedgerAdapterFunctionalTest extends FunctionalTestCase {
 
 		return getResource.path( path ).type( "application/json").get(ClientResponse.class);
 	}
+	
 	/**
 	 * Convenience method to post a request to the specified path.
 	 *
@@ -215,5 +319,25 @@ public class ILPLedgerAdapterFunctionalTest extends FunctionalTestCase {
 		}
 
 		return putResource.path( path ).type( "application/json").put(ClientResponse.class, requestData);
+	}
+
+	/**
+	 * Convenience method to make a PUT request to the specified path, with query parameters
+	 *
+	 * @param path - path to post to
+	 * @param params - Map of queryParameters
+	 * @param requestData - JSON formatted request string
+	 * @return ClientResponse instance representing the response from the service
+	 */
+	private ClientResponse putRequestWithQueryParamsNullContentType( String path, Map<String,String> params , String requestData) {
+		WebResource putResource = webService;
+
+		if ( params != null ) {
+			for ( String nextKey : params.keySet() ) {
+				putResource = putResource.queryParam( nextKey, params.get( nextKey ) );
+			}
+		}
+
+		return putResource.path( path ).type( "*/*" ).put(ClientResponse.class, requestData);
 	}
 }
