73fb8adc4d993d96718011d58eb9533740546a3d
Murthy Kakarlamudi <murthy@modusbox.com>
2017-02-23 20:46:00 -0500

    updates for basic auth

--- src/test/java/com/l1p/interop/ilp/ledger/notification/NotificationClient.java
+++ src/test/java/com/l1p/interop/ilp/ledger/notification/NotificationClient.java
@@ -16,8 +16,8 @@ public class NotificationClient {
   public static void main(String[] args) throws Exception {
     try {
       WebSocketContainer container = ContainerProvider.getWebSocketContainer();
-      //String uri = "ws://localhost:8089/websocket?token=123";
-      String uri = "ws://ec2-35-166-189-14.us-west-2.compute.amazonaws.com:8089/websocket?token=placeholder_9AtVZPN3t49Kx07stO813UHXv6pcES";
+      String uri = "ws://localhost:8089/websocket?token=123";
+      //String uri = "ws://ec2-35-166-189-14.us-west-2.compute.amazonaws.com:8089/websocket?token=placeholder_9AtVZPN3t49Kx07stO813UHXv6pcES";
       System.out.println("Connecting to " + uri);
       container.connectToServer(WebsocketClientEndpoint.class, URI.create(uri));
       messageLatch.await(300, TimeUnit.SECONDS);
