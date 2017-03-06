c7c1ec8882e2627d1f79ed5b62b02f67f8a7a045
Brian Price <priceb@Brians-MacBook-Pro.local>
2017-03-06 14:29:23 -0700

    added unit tests LedgerMetadataTransformer

--- src/test/java/com/l1p/interop/ilp/ledger/LedgerMetadataTransformerTest.java
+++ src/test/java/com/l1p/interop/ilp/ledger/LedgerMetadataTransformerTest.java
@@ -2,6 +2,7 @@ package com.l1p.interop.ilp.ledger;
 
 import static org.junit.Assert.*;
 
+import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -33,24 +34,75 @@ public class LedgerMetadataTransformerTest extends SimpleCallableJavaComponentTe
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testConstructor() {
+		String ledgerAdapterURL = "someUrl";
+		String ledgerAccountTransfersURL = "someOtherUrl";
+
+		// Exercise the constructor
+		LedgerMetadataTransformer transformer = new LedgerMetadataTransformer(ledgerAdapterURL, ledgerAccountTransfersURL);
 		
+		// verify that the constructor set the local properties correctly
+		assertEquals("ledgerAdapterURL matches", ledgerAdapterURL, transformer.ledgerAdapterURL);
+		assertEquals("ledgerAccountTransfersURL matches", ledgerAccountTransfersURL, transformer.ledgerAccountTransfersURL);
+	}
+	
+	
+	@Test
+	public void testTransform() throws Exception {
 		/* Prepare inbound message. */
-//		Object payload = buildTransformerData("userURI", "userURI");
-//		MuleEvent event = getTestEvent(payload, muleContext);
-//		MuleMessage muleMessage = event.getMessage();
-//        muleMessage.setProperty("id", "some name", PropertyScope.SESSION);
-//        
-//        GetAccountTransformer transformer = new GetAccountTransformer( createStoreData("userURI") );
-//        String response = (String) transformer.transformMessage(muleMessage, "UTF-8");
-//
-//        JSONObject jObject  = new JSONObject(response); // json
-//        JSONObject data = jObject.getJSONObject("result"); // get data object
-//        
-//        assertEquals("name of account", data.getString("name"));
-//        assertEquals("USD", data.getString("currency"));
-//        assertEquals("some account name", data.getString("account"));
+		String payload = buildTransformerData();
+		
+		String ledgerAdapterURL = "someLedgerUrl";
+		String ledgerAccountTransfersURL = "someOtherUrl";
+
+		
+		MuleEvent event = getTestEvent(payload, muleContext);
+		MuleMessage muleMessage = event.getMessage();
+        
+        System.out.println("about to transform Ledger Metadata");
+        LedgerMetadataTransformer transformer = new LedgerMetadataTransformer(ledgerAdapterURL, ledgerAccountTransfersURL );
+        System.out.println("just returend from transformer");
+        String response = (String) transformer.transformMessage(muleMessage, "UTF-8");
+        System.out.println("transformed response : " + response);
+
+        JSONObject jObject  = new JSONObject(response); // json
+        JSONObject data = jObject.getJSONObject("urls"); // get data object
+        
+        String healthUrl = (String) data.get("health");
+        System.out.println("Health URL after transform: " + healthUrl);
+        
+        assertEquals("health", data.getString("health"), ledgerAdapterURL+"/health");
+        assertEquals("transfer", data.getString("transfer"), ledgerAdapterURL+"/transfers/:id");
+        assertEquals("transfer_fulfillment", data.getString("transfer_fulfillment"), ledgerAdapterURL+"/transfers/:id/fulfillment");
+        assertEquals("transfer_state", data.getString("transfer_state"), ledgerAdapterURL+"/transfers/:id/state");
+        assertEquals("accounts", data.getString("accounts"), ledgerAdapterURL+"/accounts");
+        assertEquals("account", data.getString("account"), ledgerAdapterURL+"/accounts/:name");
+        
+        assertEquals("auth_token", data.getString("auth_token"), ledgerAdapterURL+"/auth_token");
+        assertEquals("message", data.getString("message"), ledgerAdapterURL+"/messages");
+        assertEquals("websocket", data.getString("websocket"), "ws://localhost:8089/websocket");
+        
+	}
+	
+	
+	private String buildTransformerData() throws JSONException {
+		JSONObject json = new JSONObject();
+		JSONObject urlsObject = new JSONObject();
+		
+		urlsObject.put("health", "health_url");
+		urlsObject.put("transfer", "transfer_url");
+		urlsObject.put("transfer_fulfillment", "transfer_Fullfillment_url");
+		urlsObject.put("transfer_state", "transfer_state_url");
+		urlsObject.put("accounts", "accounts_url");
+		urlsObject.put("account", "account_url");
+		
+		
+		
+		json.put("urls", urlsObject);
+		System.out.println("json object :: " + json.toString() );
+		
+		return json.toString();
+		
 	}
 
 }
