fc5b52d65198e7b57231be56972346f2271636f3
Brian Price <priceb@Brians-MacBook-Pro.local>
2017-03-09 08:36:21 -0700

    some more changes for unit testing

--- src/test/java/com/l1p/interop/ilp/ledger/notification/TestLedgerNotificationRegistrationApplication.java
+++ src/test/java/com/l1p/interop/ilp/ledger/notification/TestLedgerNotificationRegistrationApplication.java
@@ -1,27 +1,90 @@
 package com.l1p.interop.ilp.ledger.notification;
 
 import java.io.File;
-
+import java.io.IOException;
+import static org.junit.Assert.*;
 import org.apache.commons.io.FileUtils;
 import org.apache.tools.ant.taskdefs.LoadResource;
+import org.glassfish.grizzly.websockets.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mule.tck.junit4.FunctionalTestCase;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import org.glassfish.grizzly.http.HttpRequestPacket;
 
 import com.l1p.interop.ilp.ledger.LedgerUrlMapper;
 
 import junit.framework.TestCase;
 
 public class TestLedgerNotificationRegistrationApplication extends TestCase{
+	
+	private LedgerNotificationRegistrationApplication app = null;
+	
+	@Before
+	public void setUp() throws Exception {
+		System.out.println("setUp()");
+		app = new LedgerNotificationRegistrationApplication(new LedgerUrlMapper("test","test","test","test"));
+	}
 
-	@Test
-	public void testApp() throws Exception {
-		// TODO Auto-generated method stub
-				LedgerNotificationRegistrationApplication app = new LedgerNotificationRegistrationApplication(new LedgerUrlMapper("test","test","test","test"));
-				String json = FileUtils.readFileToString(new File("/Users/murthy/Clients/Gates/workspace/interop-ilp-ledger/src/test/resources/testData/rejectTransfer.json"));
-				app.sendTranferExecutedNotification(json);
-				
+	@After
+	public void tearDown() throws Exception {
+		System.out.println("tearDown()");
+		app = null;
 	}
 	
+	
+	@Test
+	public void testLoadingResourceFile() throws IOException {
+		String json = FileUtils.readFileToString(new File("src/test/resources/testData/rejectTransfer.json"));
+		assertTrue("test loading a resource file", json != null);
+		assertTrue("test loading a resource file", json.length() > 0);
+	}
+	
+	
+	@Test
+	public void testCreateSocket() {
+
+		// create needed mocks
+		ProtocolHandler handler = mock(ProtocolHandler.class);
+		HttpRequestPacket requestPacket = mock(HttpRequestPacket.class);
+		WebSocketListener listeners = mock(WebSocketListener.class);
+		
+		// perform the test
+		WebSocket webSocket = app.createSocket(handler, requestPacket, listeners);
+		
+		assertTrue("did we create a websocket", webSocket != null);
+	}
+	
+	
+	@Test
+	public void testOnConnect() {
+		// create needed mocks
+		ProtocolHandler handler = mock(ProtocolHandler.class);
+		HttpRequestPacket requestPacket = mock(HttpRequestPacket.class);
+		WebSocketListener listeners = mock(WebSocketListener.class);
+		
+		WebSocket webSocket = app.createSocket(handler, requestPacket, listeners);
+//		webSocket.
+		
+		// this is the actual test case we are performing.
+		app.onConnect(webSocket);
+		
+	}
+	
+	
+	
+	
+
+//	@Test
+//	public void testApp() throws Exception {
+//		app = new LedgerNotificationRegistrationApplication(new LedgerUrlMapper("test","test","test","test"));
+//		String json = FileUtils.readFileToString(new File("src/test/resources/testData/rejectTransfer.json"));
+//		app.sendTranferExecutedNotification(json);
+//	}
+//	
 	
 
 }
